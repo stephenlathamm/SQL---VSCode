432ab51e6cd7dee78b11464eb0f06a6a4ecb0b41
stephenlathamm <252251153+stephenlathamm@users.noreply.github.com>
2026-01-07 16:29:16 -0500

    Query 2 skills of the top paying business analyst jobs

---
+++ project_sql/2_top_paying_jobs_skills.sql
@@ -0,0 +1,44 @@
+/*
+What are the skills required for the top 10 highest paying Business Analyst roles?
+- use top 10 highest paying BA jobs from first query 
+- add the specific skills needed for these roles */
+
+
+
+
+WITH top_paying_jobs AS (
+    SELECT 
+        job_id,
+        job_title,
+        salary_year_avg,
+        name as company_name
+
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON 
+    job_postings_fact.company_id = company_dim.company_id
+
+    WHERE 
+        job_title_short = 'Business Analyst' AND 
+        job_location = 'Anywhere' AND 
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+top_paying_jobs.*,
+skills
+FROM top_paying_jobs
+
+INNER JOIN skills_job_dim ON
+top_paying_jobs.job_id = skills_job_dim.job_id
+
+INNER JOIN skills_dim ON
+skills_job_dim.skill_id = skills_dim.skill_id
+
+ORDER BY
+    salary_year_avg DESC
+
+/* we see that excel, python, and SQL are the most common skills for the top 10 highest paying BA roles */
