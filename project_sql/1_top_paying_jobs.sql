59879f2a6dfd53d5f5a289fe67d636c87d7496d5
stephenlathamm <252251153+stephenlathamm@users.noreply.github.com>
2026-01-07 16:00:56 -0500

    add sql file 2 skills of top paying jobs

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,8 +1,9 @@
 /*
-Question: What are the top paying data analyst jobs?
-- Identify the top 10 highest paying Data Analyst roles that are available remotely 
+Question: What are the top paying business analyst jobs?
+- Identify the top 10 highest paying Business Analyst roles that are available remotely 
 - Focus on job postings with specified salaries (remove nulls)
-* /
+- Include company names via a join with the company dim table
+*/
 
 SELECT 
     job_id,
@@ -10,11 +11,18 @@ SELECT
     job_location,
     job_schedule_type,
     salary_year_avg,
-    job_posted_date
+    job_posted_date,
+    name as company_name
 
 FROM
     job_postings_fact
+LEFT JOIN company_dim ON 
+job_postings_fact.company_id = company_dim.company_id
 
 WHERE 
-    job_title_short = 'Data Analyst' AND 
+    job_title_short = 'Business Analyst' AND 
     job_location = 'Anywhere' AND 
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
